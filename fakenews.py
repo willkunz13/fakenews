ebd1422a7edfe3380474620aaadc5032a597c144
jerry <wikunz13@gmail.com>
2020-08-28 16:55:52 -0400

    Add linearsvc test

--- fakenews.py
+++ fakenews.py
@@ -4,13 +4,14 @@ import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 df=pd.read_csv('~/Downloads/news.csv')
 #Get shape and head
 labels=df.label
 
-x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.1, random_state=7)
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
 
 tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.68)
 #DataFlair - Fit and transform train set, transform test set
@@ -18,10 +19,22 @@ tfidf_train=tfidf_vectorizer.fit_transform(x_train)
 tfidf_test=tfidf_vectorizer.transform(x_test)
 
 #DataFlair - Initialize a PassiveAggressiveClassifier
-pac=PassiveAggressiveClassifier(max_iter=50)
+pac=PassiveAggressiveClassifier(max_iter=50, C=1.1)
 pac.fit(tfidf_train,y_train)
 #DataFlair - Predict on the test set and calculate accuracy
 y_pred=pac.predict(tfidf_test)
+
+
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+print(confusion_matrix(y_test,y_pred, labels=['FAKE','REAL']))
+
+
+lsvc = LinearSVC(max_iter=50)
+lsvc.fit(tfidf_train, y_train)
+y_pred=lsvc.predict(tfidf_test)
+
 score=accuracy_score(y_test,y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
 
